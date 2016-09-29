ea3a25a4fea1dce58839595f6b9156389e4aeacb
luminous-io <luminous.devs@gmail.com>
2016-09-29 00:20:43 -0500

    Created basic project structure, added npm and grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // Gets name of project
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Concatenates javascript files into one semi-colon delimited file.
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/**/*.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+
+        // Launches a local web server
+        connect: {
+            options: {
+                hostname: 'localhost',
+                protocol: 'http'
+            },
+            server: {
+                options: {
+                  port: 8000,
+                  livereload: true,
+                  open: true
+                }
+            }
+        },
+
+        // Lints the javascript files
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+        },
+
+        // Uglifies!
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            dist: {
+                files: {
+                  'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+
+        // Monitors changes to files
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['build']
+        }
+    });
+
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint']);
+
+    // Other tasks.
+    grunt.registerTask('build', ['jshint', 'concat', 'uglify']);
+    grunt.registerTask('web', [
+        'build',
+        'connect:server',
+        'watch'
+    ]);
+
+};
